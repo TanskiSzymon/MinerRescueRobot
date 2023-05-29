da028ed559b03dd6ac25d2c71446a6d8b34b017d
Tansky <108231030+TanskiSzymon@users.noreply.github.com>
2023-05-30 01:36:12 +0200

    Update ESP32.cpp

--- ESP32.cpp
+++ ESP32.cpp
@@ -1,38 +1,81 @@
 #include <WiFi.h>
 #include <string>
 #include <PubSubClient.h>
+#include <string>
+#include <iostream>
+#include <algorithm>
 
 #define LMotorPWM 26 
 #define LMotorD 27 
 #define RMotorPWM 33 
 #define RMotorD 25
 #define ledPin 2
+#define TRIG_PIN 32
+#define ECHO_PIN 35
 
 clock_t start ;
 clock_t temp ;
 clock_t czas;
 long lastMsg = 0;
+long lastMsgD = 0;
 using namespace std;
 
 //Local wifi ssid and password
-const char* ssid ="wifi_ssid";   
-const char* password = "wifi_password";
+const char* ssid ="SSID";   
+const char* password = "PASSWORD";
 //Your MQTT server
 const char* mqtt_server ="broker.mqttdashboard.com";
-const char* mqtt_username = "ESPCTR0002";
+const char* mqtt_username = "ESP32_CAR2";
 const char* mqtt_password = "12345678";
-
-const char* inTopic = "PUM2023/#";
+const char* inTopic = "MQTT_PILOT/CTR";
 const int mqtt_port =1883;
-
+const int STOPdistance = 25; //cm
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+void checkDistanceAndStop() {
+long now = millis();
+  if (now - lastMsgD > 200) {
+    lastMsgD = now;
+
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  long distance = (duration/2) / 29.1;
+  string num_str = to_string(distance);
+
+  if(distance < STOPdistance) {
+    analogWrite(LMotorPWM, 0);
+    analogWrite(RMotorPWM, 0);
+    client.publish("MQTT_PILOT/ANSWER", "TO CLOSE"); 
+   }
+  }
+}
+
+void watch_dog(){
+  long now = millis();
+  if (now - lastMsg > 1000) {
+    lastMsg = now;
+
+    temp = clock();
+    czas = (double)(temp - start) / CLOCKS_PER_SEC;
+    if(czas>2){
+        analogWrite(LMotorPWM, 0);
+        analogWrite(RMotorPWM, 0);
+        client.publish("MQTT_PILOT/ANSWER", "Watch_dog stop!"); 
+      }
+  }
+} 
 
 void blink_led(unsigned int times, unsigned int duration){
   for (int i = 0; i < times; i++) {
     digitalWrite(ledPin, HIGH);
     delay(duration);
-    digitalWrite(ledPin, LOW); 
+    digitalWrite(ledPin, LOW);  
     delay(200);
   }
 }
@@ -47,8 +90,7 @@ void setup_wifi() {
 
   int c=0;
   while (WiFi.status() != WL_CONNECTED) {
-    blink_led(2,200); //blink LED twice (for 200ms ON time) to indicate that wifi not connected
-    delay(1000); //
+    blink_led(2,200); 
     Serial.print(".");
     c=c+1;
     if(c>10){
@@ -64,41 +106,35 @@ void setup_wifi() {
 }
 
 void connect_mqttServer() {
-  // Loop until we're reconnected
-
   while (!client.connected()) {
 
     if(WiFi.status() != WL_CONNECTED){
-      //if not connected, then first connect to wifi
       setup_wifi();
     }
     Serial.print("Attempting MQTT connection...");
-    String clientId = "ESPCTR0002";   // Create a random client ID
+    String clientId = "ESP_CAR2";   // Create a random client ID
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
       Serial.println("connected");
 
       client.subscribe(inTopic);   // subscribe the topics here
-      //client.subscribe(command2_topic);   // subscribe the topics here
     } else {
       temp = clock();
       czas = (double)(temp - start) / CLOCKS_PER_SEC;
-       //Serial.print( "\nSince the last message:\n");
         if(czas>2){
          analogWrite(LMotorPWM, 0);
          analogWrite(RMotorPWM, 0);
         }
       Serial.print("failed, rc=");
       Serial.print(client.state());
-      Serial.println(" try again in 3 seconds");   // Wait 5 seconds before retrying
+      Serial.println(" try again in 2 seconds");  
       blink_led(3,200);
-      delay(3000);
+      delay(2000);
     }
   }
 }
 
-//this function will be executed whenever there is data available on subscribed topics
 void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
@@ -110,7 +146,7 @@ void callback(char* topic, byte* message, unsigned int length) {
   }
   Serial.println();
 
-  if (String(topic) == "PUM2023/CTR/TEST") {
+  if (String(topic) == "MQTT_PILOT/CTR") {
       start = clock();
       int DirectionV2 = 1;
       int DirectionO2 = 1;
@@ -121,7 +157,6 @@ void callback(char* topic, byte* message, unsigned int length) {
       string VOmega;
       string tempStrV;
       for (int i = 0; (((char)message[i]!=';')&&(i < length)); i++) {
-      Serial.print((char)message[i]);
       tempStrV[i]=(char)message[i];
       messageTemp += (char)message[i];
       Vlong++;
@@ -131,60 +166,48 @@ void callback(char* topic, byte* message, unsigned int length) {
         for (int i = 1; (i < Vlong); i++){
             VVal[i-1]=tempStrV[i];
         }
-        Serial.print( "Kierunkek V - ");
       }
       if(tempStrV[0]=='+'){
         DirectionV2 = 1;
         for (int i = 1; (i < Vlong); i++){
             VVal[i-1]=tempStrV[i];
         }
-        Serial.print( "Kierunkek V + ");
       }
       if((tempStrV[0]>=48)&&(tempStrV[0]<=57)){ 
         for (int i = 0; (i < Vlong); i++){
             VVal[i]=tempStrV[i];
         }
-        Serial.print( "Kierunkek V domyslny (+) ");
       }
       for (int i = 0; (i < Vlong-1); i++){
             if((VVal[i]<48)&&(VVal[i]>57)){
               VVal="0";
             }
-      }
-      
+      } 
       RelValue = stoi(VVal);
-      Serial.print( "\nWartosc V: \n");
-      Serial.print( RelValue);
-      Serial.print( "\n");
       
       string tempStrO;
       for (int i = Vlong+1; ((i < length)); i++) {
-      //Serial.print((char)message[i]);
       tempStrO[i-(Vlong+1)]=(char)message[i];
       messageTemp += (char)message[i];
       }
-      //Serial.print( "\n\n\n");
 
       if(tempStrO[0]=='-'){
         DirectionO2 = -1;
         for (int i = 1; (i < (length-Vlong+1)); i++){
             VOmega[i-1]=tempStrO[i];
         }
-        Serial.print( "Kierunkek Omega - ");
       }
       if(tempStrO[0]=='+'){
         DirectionO2 = 1;
         for (int i = 1; (i < (length-Vlong+1)); i++){
             VOmega[i-1]=tempStrO[i];
         }
-        Serial.print( "Kierunkek Omega + ");
       }
 
       if((tempStrO[0]>=48)&&(tempStrO[0]<=57)){ 
         for (int i = 0; (i < (length-Vlong+1)); i++){
             VOmega[i]=tempStrO[i];
         }
-        Serial.print( "Kierunkek Omega domyslny (+) ");
       }  
 
       for (int i = 0; (i < (length-Vlong-2)); i++){
@@ -192,128 +215,79 @@ void callback(char* topic, byte* message, unsigned int length) {
               VOmega="0";
             }
       }
-      Serial.print( "\n");
       RelOmega = stoi(VOmega);
-      Serial.print( "\nWartosc Omega:\n");
-      Serial.print(RelOmega);
-      Serial.print( "\n");
+    int _RMotorD, _LMotorD;
+    int _RMotorPWM, _LMotorPWM;
 
-    if(DirectionV2==1){
-      if(DirectionO2>0){
-        float R = abs(RelValue * 2.55 - (RelValue * 0.01 * RelOmega * 2.55));
-        float L = abs(RelValue * 2.55) ;
-        analogWrite(LMotorPWM, (int)L);
-        digitalWrite(LMotorD, LOW);
-        analogWrite(RMotorPWM, (int)R);
-        digitalWrite(RMotorD, LOW);
-        Serial.print( "\nL: ");
-        Serial.print( L);
-        Serial.print( "\nR: ");
-        Serial.print( R);
-        Serial.print( "\n");
-      }
-      if(DirectionO2<0){
-        float L = abs(RelValue * 2.55 - (RelValue * 0.01 * RelOmega * 2.55));
-        float R = abs(RelValue * 2.55) ;
-        analogWrite(LMotorPWM, (int)L);
-        digitalWrite(LMotorD, LOW);
-        analogWrite(RMotorPWM, (int)R);
-        digitalWrite(RMotorD, LOW);
-        Serial.print( "\nL: ");
-        Serial.print( L);
-        Serial.print( "\nR: ");
-        Serial.print( R);
-        Serial.print( "\n");
-      }
-      if(RelOmega==0){
-        float L = abs(RelValue * 2.55);
-        float R = abs(RelValue * 2.55) ;
-        analogWrite(LMotorPWM, (int)L);
-        digitalWrite(LMotorD, LOW);
-        analogWrite(RMotorPWM, (int)R);
-        digitalWrite(RMotorD, LOW);
-        Serial.print( "\nL: ");
-        Serial.print( L);
-        Serial.print( "\nR: ");
-        Serial.print( R);
-        Serial.print( "\n");
-      }
+    if (RelOmega <= 20) { 
+        _RMotorD = DirectionV2 == 1 ? HIGH : LOW;  
+        _LMotorD = DirectionV2 == 1 ? HIGH : LOW;
+        
+        if (DirectionO2>0) {
+        _RMotorPWM = map(abs(RelValue - RelValue * 0.01* RelOmega), 0, 100, 0, 255);
+        _LMotorPWM = map(abs(RelValue), 0, 100, 0, 255);
+        }
+        if (DirectionO2<0) {
+        _RMotorPWM = map(abs(RelValue), 0, 100, 0, 255);
+        _LMotorPWM = map(abs(RelValue - RelValue * 0.01* RelOmega), 0, 100, 0, 255);
+
+        }
+    } else { 
+        _RMotorD = DirectionO2 == 1 ? HIGH : LOW;  
+        _LMotorD = DirectionO2 == 1 ? LOW : HIGH; 
+        _RMotorPWM = map(RelOmega, 20, 100, 130, 200);
+        _LMotorPWM = map(RelOmega, 20, 100, 130, 200);
     }
-    if(DirectionV2<0){
-      if(DirectionO2>0){
-        float L = abs(RelValue * 2.55 - (RelValue * 0.01 * RelOmega * 2.55));
-        float R = abs(RelValue * 2.55) ;
-        analogWrite(LMotorPWM, (int)L);
-        digitalWrite(LMotorD, HIGH);
-        analogWrite(RMotorPWM, (int)R);
-        digitalWrite(RMotorD, HIGH);
-        Serial.print( "\nL: ");
-        Serial.print( L);
-        Serial.print( "\nR: ");
-        Serial.print( R);
-        Serial.print( "\n");
+      
+      digitalWrite(TRIG_PIN, LOW);
+      delayMicroseconds(2);
+      digitalWrite(TRIG_PIN, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(TRIG_PIN, LOW);
+      long duration = pulseIn(ECHO_PIN, HIGH);
+      long distance = (duration/2) / 29.1;
+      if((distance>=STOPdistance)||((_RMotorD==HIGH)&&(_RMotorD==HIGH))){
+        analogWrite(LMotorPWM, _LMotorPWM);
+        analogWrite(RMotorPWM, _RMotorPWM);
+        digitalWrite(LMotorD, _LMotorD);
+        digitalWrite(RMotorD, _RMotorD);
       }
-      if(DirectionO2<0){
-        float R = abs(RelValue * 2.55 - (RelValue * 0.01 * RelOmega * 2.55));
-        float L = abs(RelValue * 2.55) ;
-        analogWrite(LMotorPWM, (int)L);
-        digitalWrite(LMotorD, HIGH);
-        analogWrite(RMotorPWM, (int)R);
-        digitalWrite(RMotorD, HIGH);
-        Serial.print( "\nL: ");
-        Serial.print( L);
-        Serial.print( "\nR: ");
-        Serial.print( R);
-        Serial.print( "\n");
+      else {
+        analogWrite(LMotorPWM, 0);
+        analogWrite(RMotorPWM, 0);
+        digitalWrite(LMotorD, LOW);
+        digitalWrite(RMotorD, LOW);
       }
-      if(RelOmega==0){
-        float L = abs(RelValue * 2.55);
-        float R = abs(RelValue * 2.55) ;
-        analogWrite(LMotorPWM, (int)L);
-        digitalWrite(LMotorD, HIGH);
-        analogWrite(RMotorPWM, (int)R);
-        digitalWrite(RMotorD, HIGH);
-        Serial.print( "\nL: ");
-        Serial.print( L);
-        Serial.print( "\nR: ");
-        Serial.print( R);
-        Serial.print( "\n");
-      }
+      Serial.print( "\nL Value -  ");
+      Serial.print(_LMotorPWM);
+      Serial.print( "\nR Valiue -  ");
+      Serial.print(_RMotorPWM);
     }
   }
-}
-
 void setup() {
   Serial.begin(921600);
-  pinMode(LMotorPWM, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
   pinMode(LMotorPWM, OUTPUT);
   pinMode(RMotorPWM, OUTPUT);
   pinMode(LMotorD, OUTPUT);
   pinMode(RMotorD, OUTPUT);
   pinMode(ledPin, OUTPUT);
+  blink_led(1, 3000);
   setup_wifi();
   client.setServer(mqtt_server,mqtt_port);
   client.setCallback(callback);
+  blink_led(2, 500);
 }
 
 void loop() {
-  
   if (!client.connected()) {
+    analogWrite(LMotorPWM, 0);
+    analogWrite(RMotorPWM, 0);
     connect_mqttServer();
   }
   client.loop();
-  
-  long now = millis();
-  if (now - lastMsg > 4000) {
-    lastMsg = now;
-
-    temp = clock();
-    czas = (double)(temp - start) / CLOCKS_PER_SEC;
-    //Serial.print( "\nSince the last message:\n");
-    if(czas>2){
-        analogWrite(LMotorPWM, 0);
-        analogWrite(RMotorPWM, 0);
-      }
-    client.publish("PUM2023/CTR/Answer", "ok"); 
-  }
+  checkDistanceAndStop();
+  watch_dog();
+  delay(50);
 }
